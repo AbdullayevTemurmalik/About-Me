c34c19af4442c27bf926b10f679065b2b1770a52
TemurMalik <tmaq77@gmail.com>
2026-02-17 10:29:51 +0500

    add overill

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Header from "./components/Header";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Aboutme from "./components/Aboutme";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/about" element={<Aboutme />} />
+        <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/" element={<Aboutme />} />
+        <Route path="/" element={<About />} />
       </Routes>
     </Router>
   );
